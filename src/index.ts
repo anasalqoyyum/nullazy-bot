e3efc0b8d690aaf04b7e84d658a8b167a74e87f9
Anas Alqoyyum <muhanasalqoyyum@gmail.com>
2022-04-16 22:39:43 +0700

    feat: add slash commands builder and cat 🐈

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
+import axios from "axios";
 import { Client, Intents } from "discord.js";
-import { token } from "./utils/env";
+import env from "./utils/env";
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // When the client is ready, run this code (only once)
 client.once("ready", () => {
-  console.log("Ready!");
+  console.log("Bot is currently online!");
 });
 
 client.on("interactionCreate", async (interaction) => {
@@ -19,10 +20,27 @@ client.on("interactionCreate", async (interaction) => {
       await interaction.reply("Pong!");
       break;
     case "server":
-      await interaction.reply(`Server name: ${interaction.guild?.name}\nTotal members: ${interaction.guild?.memberCount}`);
+      await interaction.reply(
+        `Server name: ${interaction.guild?.name}\nTotal members: ${interaction.guild?.memberCount}`
+      );
       break;
     case "user":
-      await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+      await interaction.reply(
+        `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
+      );
+      break;
+    case "cat":
+      await interaction.deferReply();
+      const res = await axios.get(
+        "https://api.thecatapi.com/v1/images/search?limit=1",
+        {
+          headers: {
+            "x-api-key": env.catToken,
+          },
+        }
+      );
+      const imageUrl = res.data[0].url;
+      interaction.editReply({ files: [imageUrl] });
       break;
     default:
       console.log("Sorry, we are out of command.");
@@ -30,4 +48,4 @@ client.on("interactionCreate", async (interaction) => {
 });
 
 // Login to Discord with your client's token
-client.login(token);
+client.login(env.token);
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,5 +1,10 @@
 import "dotenv/config";
 
-export const token = process.env.DISCORD_TOKEN || ""
-export const clientId = process.env.CLIENT_ID || ""
-export const guildId = process.env.GUILD_ID || ""
+const env = {
+  token: process.env.DISCORD_TOKEN || "",
+  clientId: process.env.CLIENT_ID || "",
+  guildId: process.env.GUILD_ID || "",
+  catToken: process.env.CAT_API_TOKEN || "",
+};
+
+export default env;
